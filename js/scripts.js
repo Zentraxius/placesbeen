5e74339105dde3442a2fb829c6c684dd96352fae
Cody Fritz <clanalia55@gmail.com>
2020-06-15 16:20:56 -0700

    update js code on business logic
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,28 @@
 
 let vacation = new Vacation();
 
-$(document).ready(function() {
-  $('#nyClick').click(function(event) {
-    event.preventDefault();
+let nyc = new Place ("New York", "Statue of Liberty", "June 10th, 2020", "Lots of people")
+let honolulu = new Place ("Hawaii","Diamond Head", "Jan 15th, 1998", "Less sharks than expected")
+let lackland = new Place ("Texas", "Lackland Air Force Base", "August 4th, 2014", "Suffering builds character")
 
-  function displayPlaceDetails(vacationToDisplay) {
-  let vacationList = $("ul#places");
-  let htmlForPlaceInfo = "";
-  vacationToDisplay.places.forEach(function(place) {
-    htmlForPlaceInfo += "<li" + place.state + " " + place.landmark + " " + place.date + " " + place.note + "</li>";
-  });
-  vacationList.html(htmlForPlaceInfo);
+///////
+
+function displayPlaceDetails(Vacation) {
+let vacationList = $("ul#places");
+let htmlForPlaceInfo = "";
+Vacation.places.forEach(function(place) {
+  htmlForPlaceInfo += "<li>" + place.state + " " + place.landmark + " " + place.date + " " + place.note + "</li>";
+});
+vacationList.html(htmlForPlaceInfo);
 };
-})
-})
+
+$(document).ready(function() {
+  $('#click').click(function(event) {
+  event.preventDefault();
+  
+  displayPlaceDetails(Vacation);
+});
+});
 
 ///// Business Logic /////
 
@@ -25,9 +33,6 @@ function Place(state, landmark, date, note) {
   this.date = date;
   this.note = note
 }
-let nyc = new Place ("New York", "Statue of Liberty", "June 10th, 2020", "Lots of people")
-let honolulu = new Place ("Hawaii","Diamond Head", "Jan 15th, 1998", "Less sharks than expected")
-let lackland = new Place ("Texas", "Lackland Air Force Base", "August 4th, 2014", "Suffering builds character")
 
 Place.prototype.infoMe = function () {
   return this.state + " " + this.landmark + " " + this.date + " " + this.note;
