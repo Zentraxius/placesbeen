4e12b9b5a176de72c4daf3d0719f5f18c9e133e9
Cody Fritz <clanalia55@gmail.com>
2020-06-15 14:55:19 -0700

    update js code on business logic
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,21 @@ function Place(state, landmark, date, note) {
   this.date = date;
   this.note = note
 }
+let nyc = new Place ("New York", "Statue of Liberty", "June 10th, 2020", "Lots of people")
+let honolulu = new Place ("Hawaii","Diamond Head", "Jan 15th, 1998", "Less sharks than expected")
+let lackland = new Place ("Texas", "Lackland Air Force Base", "August 4th, 2014", "Suffering builds character")
 
 Place.prototype.infoMe = function () {
   return this.state + " " + this.landmark + " " + this.date + " " + this.note;
 };
 
 ///// More Business Logic /////
-
 function Vacation() {
   this.places = [];
 }
+Vacation.prototype.addPlace = function(place) {
+  this.places.push(place);
+};
+
+let vacation = new Vacation();
 
